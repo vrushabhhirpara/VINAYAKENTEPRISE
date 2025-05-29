49297638bf09f092ef24168a3a8fd3efddb00271
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-05-29 14:09:13 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -284,7 +284,7 @@ const FeaturedProducts: React.FC = () => {
       id: 4,
       name: "Paint, Ink & Coatings",
       category: "Paint Colour",
-      image: "https://www.pciplindia.com/webfiles/Industry/Medium/30342020033427Paint_text.webpauto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+      image: "https://www.pciplindia.com/webfiles/Industry/Medium/30342020033427Paint_text.webp,
       description: "Effective chemicals for water purification and treatment systems."
     }
   ];
