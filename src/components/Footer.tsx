0bfb7400010a01a83b538d5ddfe153643628f658
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-05-29 14:59:44 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -124,7 +124,7 @@ const Footer: React.FC = () => {
         </div>
 
         <div className="mt-12 pt-8 border-t border-gray-700 text-center text-gray-400">
-          <p>© {new Date().getFullYear()} VinayakEnterprise. All rights reserved.</p>
+          <p>© {new Date().getFullYear()} VinayakEnterprise. All rights reserved.                                                       Powered By ANADI TECH</p>
         </div>
       </div>
     </footer>
