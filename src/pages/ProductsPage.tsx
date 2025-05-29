eba77b15fb3bd4f842aedb4b27d85f959308ee97
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-05-29 14:46:20 +0530

    Updated ProductsPage.tsx

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -105,7 +105,7 @@ const ProductsPage: React.FC = () => {
     {
       id: 10,
       name: "Cattle & Poultry Feed",
-      category: "Ceramics",
+      category: "Animal Feed",
       subcategory: "Processing Aids",
       image: "https://images.pexels.com/photos/357428/pexels-photo-357428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       description: "Specialized chemicals for ceramic manufacturing processes.",
