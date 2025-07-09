cd546cd9c339bbecb9585b5198b06c3f846800ce
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-07-09 18:48:56 +0530

    Updated ProductDetailPage.tsx

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -304,7 +304,7 @@ const ProductDetailPage: React.FC = () => {
     },
     {
       id: 112,
-      name: "Cyclohexane",
+      name: "Poly Acrylic Acid",
       category: "Industrial Solvents",
       subcategory: "Aliphatics",
       image: "https://images.pexels.com/photos/2280547/pexels-photo-2280547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
