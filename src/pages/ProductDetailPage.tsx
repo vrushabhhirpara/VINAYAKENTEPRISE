57fee363bf3e22ce9cc8e84eb2c168d53ab17831
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-07-09 18:53:45 +0530

    Updated ProductDetailPage.tsx

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -458,7 +458,7 @@ const ProductDetailPage: React.FC = () => {
     },
     {
       id: 119,
-      name: "Dimethylformamide",
+      name: "Glacial Acetic Acid",
       category: "Industrial Solvents",
       subcategory: "Amides",
       image: "https://images.pexels.com/photos/2280547/pexels-photo-2280547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
