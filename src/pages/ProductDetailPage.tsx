73594acc4af20b9632f9d418f847f2823d64fb04
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-07-09 19:54:03 +0530

    Updated ProductDetailPage.tsx

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -2029,7 +2029,7 @@ const ProductDetailPage: React.FC = () => {
     {
       id: 6,
       name: "Ferrous Sulphate",
-      category: "Cosmetic & Personal Care",
+      category: "Agriculture",
       subcategory: "Surfactants",
       image: "https://cdn.shopify.com/s/files/1/0646/1551/4330/files/Importance_of_Personal_Care_Products_480x480.webp?v=1673811372",
       description: "Anionic surfactant widely used in personal care and cleaning products.",
