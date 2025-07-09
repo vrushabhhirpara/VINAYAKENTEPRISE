cf1ed355dfe2c3f164829861d15e286c6d31c15d
vrushabhhirpara <132201542+vrushabhhirpara@users.noreply.github.com>
2025-07-09 19:39:24 +0530

    Updated ProductDetailPage.tsx

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -614,7 +614,7 @@ const ProductDetailPage: React.FC = () => {
     },
     {
       id: 205,
-      name: "Cocamidopropyl Betaine",
+      name: "Acid Slurry Labsa",
       category: "Cosmetic & Personal Care",
       subcategory: "Surfactants",
       image: "https://cdn.shopify.com/s/files/1/0646/1551/4330/files/Importance_of_Personal_Care_Products_480x480.webp?v=1673811372",
